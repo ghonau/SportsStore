aecccd8496482cc70f76394c6600fb30fe7d51fb
ghonau <gholamreza.fathpour@gmail.com>
2015-02-04 16:53:45 +1030

    Adding database connection

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+using System.Linq; 
+using Moq;
+using SportsStore.WebUI.Controllers; 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using SportsStore.Domain.Entities;
+using System.Collections;
+using System.Collections.Generic; 
 
 namespace SportsStore.Tests
 {
@@ -7,8 +15,30 @@ namespace SportsStore.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+             //Arrange 
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]{
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            });
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3; 
+            
+            //Act 
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model; 
+
+            //Assert 
+            Product[] prodArray = result.ToArray();
+
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5"); 
+
         }
     }
 }
